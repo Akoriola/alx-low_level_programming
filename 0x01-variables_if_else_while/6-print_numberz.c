dd76d36dbc110054d2d0db81e8e82dbe5090429a
Akoriola <olasunkanmidara01@gmail.com>
2023-03-20 16:23:18 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - output all single digit number of base 10
+ *
+ * Return:0 if sucessful
+ */
+int main(void)
+
+{
+	int sly;
+
+	for (sly = 0; sly < 10; sly++)
+		putchar((sly % 10) + '0');
+
+
+	putchar('\n');
+
+	return (0);
+}
