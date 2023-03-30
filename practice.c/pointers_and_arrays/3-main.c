3276bf8fbb3c8520d4c2ff7cc0978cb8598f4a01
Akoriola <olasunkanmidara01@gmail.com>
2023-03-30 14:57:29 -0700

    pointers

---
+++ practice.c/pointers_and_arrays/3-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing the address of a pointer
+ *
+ * return: Always 0
+ */
+int main(void)
+{
+	int *p;
+
+	printf("Address of the variable 'p': %p\n", &p);
+	return (0);
+}
